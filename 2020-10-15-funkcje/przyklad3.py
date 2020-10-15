5ceb36a3f1842aefb32d356295b0d9f88dbcdb0d
Mateusz Adamowski <mateusz@adamowski.pl>
2020-10-15 18:43:58 +0200

    zadanie - F

--- 2020-10-15-funkcje/przyklad3.py
+++ 2020-10-15-funkcje/przyklad3.py
@@ -10,3 +10,7 @@ for tempC in temperaturyC:
     tempF = przelicz_C_na_F( tempC )
     tempK = przelicz_C_na_K( tempC )
     print(f"{tempC:8.1f}°C = {tempF:8.1f}°F = {tempK:8.2f}°K ")
+
+# dopisać brakujący kod przeliczający F na C i K
+
+temperaturyF = [ -2, 0, 2, 8, 10, 20, 25, 30, 40, 50, 60, 70 ]
