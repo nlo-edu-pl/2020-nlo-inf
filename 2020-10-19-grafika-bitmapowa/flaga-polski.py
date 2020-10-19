e517b0e10af53cbc80259d8dbde6ecca71247041
Mateusz Adamowski <mateusz@adamowski.pl>
2020-10-19 18:44:13 +0200

    flaga Polski

---
+++ 2020-10-19-grafika-bitmapowa/flaga-polski.py
@@ -0,0 +1,13 @@
+width = 320
+height = 200
+
+with open("polska.ppm","w") as f:
+    f.write("P3\n")
+    f.write(f"{width} {height}\n")
+    f.write("255\n")
+    for y in range(height):
+        for x in range(width):
+            if y < height / 2:
+                f.write(f"{255} {255} {255}\n")
+            else:
+                f.write(f"{212} {33} {61}\n")
