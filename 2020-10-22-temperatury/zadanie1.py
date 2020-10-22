d563c618107a4fcfc3125016e991fd510674e74a
Mateusz Adamowski <mateusz@adamowski.pl>
2020-10-22 17:37:27 +0200

    zadanie 1 - konwersja temperatur

---
+++ 2020-10-22-temperatury/zadanie1.py
@@ -0,0 +1,19 @@
+# Zaimplementować funkcję przeliczającą temperatury obsłgującą 4 skale:
+# - Celsjusza
+# - Kelvina
+# - Fahrenheita
+# - Rankine'a
+
+
+x = przelicz_temp( 0, "C", "F" )
+print(x)
+# powinno wyjsc 32
+
+x = przelicz_temp( 100, "C", "F" )
+print(x)
+# powinno wyjsc 212
+
+x = przelicz_temp( 0, "K", "C" )
+print(x)
+# powinno wyjsc -273.15
+
