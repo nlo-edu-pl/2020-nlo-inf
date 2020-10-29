18ded41ecd9c6e1ac232244880c7734ce3066540
Mateusz Adamowski <mateusz@adamowski.pl>
2020-10-29 14:55:13 +0100

    sprawdzamy tylko do pierwiastka z tej liczby

--- 2020-10-29-liczby-pierwsze/czypierwsza.py
+++ 2020-10-29-liczby-pierwsze/czypierwsza.py
@@ -1,11 +1,13 @@
 def czy_pierwsza( n ):
     for i in range( 2, n ):
-        #print( f"{n} modulo {i} = {n % i} ")
+        #print( f"{n} / {i} = { n / i } (reszta {n % i}) ")
         if n % i == 0:
             return False
+        if i * i > n:
+            break
     return True
 
-liczba = 127
+liczba = 4083668327423
 if czy_pierwsza( liczba ):
     print("Liczba jest pierwsza")
 else:
