e750724dec546e69137410c924fddfaa35ed07de
Mateusz Adamowski <mateusz@adamowski.pl>
2020-11-23 17:54:10 +0100

    zmiana funkcji na metody klasy Uczen

--- 2020-11-19/przyklad1.py
+++ 2020-11-19/przyklad1.py
@@ -1,52 +1,32 @@
-uczniowie = [
-  { "imie" : "Katarzyna", "nazwisko" : "U.", "oceny" : "4 4 4 4 4 5 5 3 3 4 3 4" },
-  { "imie" : "Małgorzata", "nazwisko" : "K.", "oceny" : "3 3 4 4 2 2 3 4 3 3 3 2" },
-  { "imie" : "Wiktor", "nazwisko" : "J.", "oceny" : "4 3 4 3 3 4 4 3 3 3 3 3" },
-  { "imie" : "Wiktor", "nazwisko" : "N.", "oceny" : "4 3 2 3 5 2 5 5 3 3 5 5" },
-  { "imie" : "Gabrysia", "nazwisko" : "J.", "oceny" : "3 4 4 3 4 3 3 4 4 3 4 4" },
-  { "imie" : "Katarzyna", "nazwisko" : "O.", "oceny" : "2 5 2 2 3 4 5 4 2 4 4 2" },
-  { "imie" : "Jarema", "nazwisko" : "Z.", "oceny" : "4 3 4 1 2 3 4 5 5 2 2 4" },
-  { "imie" : "Marek", "nazwisko" : "D.", "oceny" : "5 5 2 5 4 3 4 5 3 4 3 4" },
-  { "imie" : "Marek", "nazwisko" : "D.", "oceny" : "5 5 4 3 4 4 4 3 3 3 5 3" },
-  { "imie" : "Marek", "nazwisko" : "X.", "oceny" : "2 2 3 2 2 2 3 3 4 3 3 2" },
-  { "imie" : "Marek", "nazwisko" : "B.", "oceny" : "6 4 5 6 5 5 6 6 6 6 5 6" },
-  { "imie" : "Marek", "nazwisko" : "V.", "oceny" : "4 5 3 3 2 4 4 5 3 6 2 3" }
-]
-
 class Uczen:
-    def __init__(self,imie):
-        self.imie = imie
+    def __init__(self, imie, nazwisko, oceny):
+        self._imie = imie
+        self._nazwisko = nazwisko
+        self._oceny = oceny
         print(f"Utworzono nowego ucznia ({imie})!")
     def powitanie(self):
-        print(f"Dzien dobry, nazywam sie {self.imie}")
-    imie = "bezimienny"
+        print(f"Dzien dobry, nazywam sie {self._imie} {self._nazwisko} {self._oceny}")
+    def policz_srednia(self):
+        return sum(self._oceny)/len(self._oceny)
+    def wyswietl_ucznia(self):
+        srednia = self.policz_srednia()
+        if srednia > 4.7:
+            pasek = "czerwony pasek"
+        else:
+            pasek = ""
+        print(f"{self._imie} {self._nazwisko} srednia: {srednia} {pasek}")
+    _imie = None
+    _nazwisko = None
+    _oceny = None
+    _adres = None
 
-u1 = Uczen("Marcin")
-u2 = Uczen("Karol")
-#u3 = Uczen( "Ania", "Xyz", [3,4,5,6,6,6,5,6,5] )
+u1 = Uczen("Marcin", "xxxx", [3,5,4,3])
+u2 = Uczen("Karol", "yyyyy", [3,3,3,3,3])
+u3 = Uczen( "Ania", "Xyz", [3,4,5,6,6,6,5,6,5] )
 u1.powitanie()
 u2.powitanie()
+u3.powitanie()
 
-# dopisac do konstruktora obsluge nazwiska i listy ocen
-# np.
-# u3 = Uczen( "Ania", "Xyz", [3,4,5,6,6,6,5,6,5] )
-
-
-def wyswietl_ucznia( uczen ):
-    srednia = policz_srednia( uczen )
-    if srednia > 4.7:
-        pasek = "czerwony pasek"
-    else:
-        pasek = ""
-    print(f"{uczen['imie']} {uczen['nazwisko']} srednia: {srednia} {pasek}")
-
-def policz_srednia( uczen ):
-    oceny_t = uczen['oceny'].split()
-    oceny = []
-    for o in oceny_t:
-        oceny.append( int(o) )
-    return sum(oceny)/len(oceny)
-
-#for u in uczniowie:
-#    wyswietl_ucznia( u )
+for u in ( u1, u2, u3 ):
+    u.wyswietl_ucznia()
 
