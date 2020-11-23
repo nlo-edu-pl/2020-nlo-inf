5949c4659ffc468ab98eddb8f0d9f87a2a307135
Mateusz Adamowski <mateusz@adamowski.pl>
2020-11-23 18:13:32 +0100

    implementacja i poprawka

--- 2020-11-23-obiektowosc/kwadrat1.py
+++ 2020-11-23-obiektowosc/kwadrat1.py
@@ -1,11 +1,13 @@
 class Kwadrat:
-    # ZADANIE:
-    # DOPISAĆ IMPLEMENTACJĘ KLASY Kwadrat
-    pass
-
+    def __init__(self, bok):
+        self._bok = bok
+    def obwod(self):
+        return self._bok * 4
+    def pole(self):
+        return self._bok**2
 
 kw = Kwadrat(4.5)
 p = kw.pole()
-obw = kw.obw()
+obw = kw.obwod()
 
 print(f"Kwadrat ma pole={p} i obwód={obw}")
