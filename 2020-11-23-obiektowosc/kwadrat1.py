088130eefcd3a87aa6409370de8b7af14f771286
Mateusz Adamowski <mateusz@adamowski.pl>
2020-11-23 17:58:51 +0100

    kwadrat

---
+++ 2020-11-23-obiektowosc/kwadrat1.py
@@ -0,0 +1,11 @@
+class Kwadrat:
+    # ZADANIE:
+    # DOPISAĆ IMPLEMENTACJĘ KLASY Kwadrat
+    pass
+
+
+kw = Kwadrat(4.5)
+p = kw.pole()
+obw = kw.obw()
+
+print("Kwadrat ma pole={p} i obwód={obw}")
