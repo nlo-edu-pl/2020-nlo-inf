e3888ef50724eff68dfd0f9f5020737b03e46055
Mateusz Adamowski <mateusz@adamowski.pl>
2020-11-26 18:23:42 +0100

    praca domowa - szyfr cezara

---
+++ 2020-11-26-szyfry/szyfr_cezara.py
@@ -0,0 +1,12 @@
+# Praca domowa
+
+def szyfr_cezara( klucz, napis ):
+    wynik = ""
+    # tutaj trzeba zaimplementować funkcję
+    return wynik
+
+k = int(input("Podaj klucz: "))
+n = input("Podaj wiadomość:")
+z = szyfr_cezara( k, n )
+print(f"Po zaszyfrowaniu: {z}")
+
