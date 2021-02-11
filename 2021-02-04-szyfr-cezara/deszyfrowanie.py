80aa5640adabc96646fd8028b3fd4cdbd0c557a8
Mateusz Adamowski <mateusz@adamowski.pl>
2021-02-11 15:47:41 +0100

    deszyfrowanie

--- 2021-02-04-szyfr-cezara/deszyfrowanie.py
+++ 2021-02-04-szyfr-cezara/deszyfrowanie.py
@@ -1,17 +1,24 @@
 # Tabela ASCII: https://en.wikipedia.org/wiki/ASCII
 
 
-napis = "CBUDIB ZFKRGCD GR VCDIB"
+napis = "Cbudib Zfkrgcd GR VCDIB"
 wynik = ""
 
 for x in napis:
     if x.isalpha():
         kodlitery = ord(x)
-        numerlitery = kodlitery - 65
-        numerliterysz = numerlitery - 3
-        if numerliterysz < 0:
-            numerliterysz = numerliterysz + 26
-        kodliterysz = numerliterysz + 65
+        if kodlitery >= 65 and kodlitery <= 90:
+            numerlitery = kodlitery - 65
+            numerliterysz = numerlitery - 3
+            if numerliterysz < 0:
+                numerliterysz = numerliterysz + 26
+            kodliterysz = numerliterysz + 65
+        else:
+            numerlitery = kodlitery - 97
+            numerliterysz = numerlitery - 3
+            if numerliterysz < 0:
+                numerliterysz = numerliterysz + 26
+            kodliterysz = numerliterysz + 97
         literasz = chr(kodliterysz)
         #print(x, kodlitery, numerlitery, numerliterysz, kodliterysz, literasz)
         wynik = wynik + literasz
