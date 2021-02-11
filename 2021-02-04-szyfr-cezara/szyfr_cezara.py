0a0dbe9a48c967fb640dfd10c2844679e88aecb1
Mateusz Adamowski <mateusz@adamowski.pl>
2021-02-11 16:08:15 +0100

    szyfr cezara - koniec

--- 2021-02-04-szyfr-cezara/szyfr_cezara.py
+++ 2021-02-04-szyfr-cezara/szyfr_cezara.py
@@ -7,19 +7,19 @@ def szyfr_cezara(klucz, napis):
             kodlitery = ord(x)
             # sprawdzamy, czy mamy mala czy wielka litere
             # sprawdzić, czy kod litery >= 65 oraz <= 90
-            if kodlitery >= 65 and kodlitery <= 90:
-                numerlitery = kodlitery - 65
-                numerliterysz = numerlitery + klucz
-                if numerliterysz > 25:
-                    numerliterysz = numerliterysz - 26
-                kodliterysz = numerliterysz + 65
-            else:
-                numerlitery = kodlitery - 97
-                numerliterysz = numerlitery + klucz
-                if numerliterysz > 25:
-                    numerliterysz = numerliterysz - 26
-                kodliterysz = numerliterysz + 97
-                
+            
+            koda = ord('A' if 'A' <= x <= 'Z' else 'a')
+
+            numerlitery = kodlitery - koda
+
+            numerliterysz = numerlitery + klucz
+            if numerliterysz > 25:
+                numerliterysz = numerliterysz - 26
+            if numerliterysz < 0:
+                numerliterysz = numerliterysz + 26
+
+            kodliterysz = numerliterysz + koda
+
             literasz = chr(kodliterysz)
             #print(x, kodlitery, numerlitery, numerliterysz, kodliterysz, literasz)
             wynik = wynik + literasz
