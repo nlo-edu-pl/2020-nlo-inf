39fe3eab555ddc8fdfa31a556b57699dd5c360d3
Mateusz Adamowski <mateusz@adamowski.pl>
2021-02-11 15:46:28 +0100

    obsługa mały i wielkich liter

--- 2021-02-04-szyfr-cezara/szyfrowanie.py
+++ 2021-02-04-szyfr-cezara/szyfrowanie.py
@@ -1,17 +1,27 @@
 # Tabela ASCII: https://en.wikipedia.org/wiki/ASCII
 
 
-napis = "ZYRAFY WCHODZA DO SZAFY"
+napis = "Zyrafy Wchodza DO SZAFY"
 wynik = ""
 
 for x in napis:
     if x.isalpha():
         kodlitery = ord(x)
-        numerlitery = kodlitery - 65
-        numerliterysz = numerlitery + 3
-        if numerliterysz > 25:
-            numerliterysz = numerliterysz - 26
-        kodliterysz = numerliterysz + 65
+        # sprawdzamy, czy mamy mala czy wielka litere
+        # sprawdzić, czy kod litery >= 65 oraz <= 90
+        if kodlitery >= 65 and kodlitery <= 90:
+            numerlitery = kodlitery - 65
+            numerliterysz = numerlitery + 3
+            if numerliterysz > 25:
+                numerliterysz = numerliterysz - 26
+            kodliterysz = numerliterysz + 65
+        else:
+            numerlitery = kodlitery - 97
+            numerliterysz = numerlitery + 3
+            if numerliterysz > 25:
+                numerliterysz = numerliterysz - 26
+            kodliterysz = numerliterysz + 97
+            
         literasz = chr(kodliterysz)
         #print(x, kodlitery, numerlitery, numerliterysz, kodliterysz, literasz)
         wynik = wynik + literasz
