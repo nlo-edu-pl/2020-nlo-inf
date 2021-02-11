908984ba25c30e3c6b5bb77c3f6c5fa5b39454cd
Mateusz Adamowski <mateusz@adamowski.pl>
2021-02-11 14:47:17 +0100

    szyfr cezara

--- 2021-02-04-szyfr-cezara/szyfr_cezara.py
+++ 2021-02-04-szyfr-cezara/szyfr_cezara.py
@@ -1,19 +1,14 @@
 # Praca domowa
 
-def szyfr_cezara( klucz, napis ):
+def szyfr_cezara(klucz, napis):
     wynik = ""
-    # tutaj trzeba zaimplementować funkcję
+
     return wynik
 
 k = int(input("Podaj klucz: "))
 n = input("Podaj wiadomość:")
-z = szyfr_cezara( k, n )
+z = szyfr_cezara(k, n)
 print(f"Po zaszyfrowaniu: {z}")
 
-# PRZYKŁAD:
-# n = "Ala ma kota"
-# k = 3
-# wynik: "Fqf rf ptyf"
-#
 # Szyfr Cezara w przeglądarce:
 # https://cryptii.com/pipes/caesar-cipher
