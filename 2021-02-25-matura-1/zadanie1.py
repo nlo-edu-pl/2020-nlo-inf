9b981b2dfe0db4d0364d24746b5a897a2a4cfe02
Mateusz Adamowski <mateusz@adamowski.pl>
2021-02-25 14:54:16 +0100

    zad 1

---
+++ 2021-02-25-matura-1/zadanie1.py
@@ -0,0 +1,20 @@
+# A jest listą w pythonie (list)
+# lista w pythonie jest podobna do tablicy (array) z innych języków programowania
+
+
+A = [6, 70, 12, 13, 15, 20]
+
+B = [13, 15, 20, 6, 70, 12]
+
+print(A[0]) # to będzie 6
+print(A[1]) # to będzie 70
+print(A[2]) # to będzie 12
+# itd
+
+print(A[:3])
+print(B[3:])
+print(A[:3] == B[3:])
+
+
+
+
