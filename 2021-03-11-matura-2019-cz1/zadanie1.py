c3297f2b9d7bd477388f4f005d91b651870760c5
Mateusz Adamowski <mateusz@adamowski.pl>
2021-03-11 15:03:50 +0100

    generowanie danych losowych

--- 2021-03-11-matura-2019-cz1/zadanie1.py
+++ 2021-03-11-matura-2019-cz1/zadanie1.py
@@ -1,8 +1,22 @@
+import random
 # lista liczb do zbadania:
 
-A = [7, 5, 9, 13, 11, 17, 21, 23, 42, 44, 80, 90, 2]
+def losowe_liczby_jasia_i_malgosi(ile_m, ile_j):
+    wynik = []
+    for i in range(ile_m):
+        wynik.append(random.randint(1,1000)*2+1)
+    for i in range(ile_j):
+        wynik.append(random.randint(1,1000)*2)
+    return wynik
+#A = [7, 5, 9, 13, 11, 17, 21, 23, 42, 44, 80, 90, 2]
 
+A = losowe_liczby_jasia_i_malgosi(1000000,7)
+
+#print(A)
 for liczba in A:
     if liczba % 2 == 0:
-        print(liczba)
+        w = liczba
         break
+
+print(w)
+
