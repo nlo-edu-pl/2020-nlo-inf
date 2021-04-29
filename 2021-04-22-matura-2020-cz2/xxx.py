780bf60770856d06c8ef3b43c69694f083e7e9e0
Mateusz Adamowski <mateusz@adamowski.pl>
2021-04-29 14:38:05 +0200

    przykład umieszczania dokumentacji (str) w kodzie

---
+++ 2021-04-22-matura-2020-cz2/xxx.py
@@ -0,0 +1,21 @@
+"""
+Moduł stworzony na zaliczenie informatyki na 2
+"""
+
+def xyz(n):
+    """
+    Funkcja xyz(n) zwraca liczbę trzy razy większą od n
+
+    Przykład użycia:
+    >>> xyz(10)
+    30
+    >>> xyz(1000)
+    3000
+    """
+    return 3*n
+
+def abc(n):
+    """
+    Funkcja abc zwraca liczbę o 4 mniejszą niż n
+    """
+    return n - 4
