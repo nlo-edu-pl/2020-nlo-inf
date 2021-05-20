8c5d981edfc4beefdcf44b9313a4ee0657b24018
Mateusz Adamowski <mateusz@adamowski.pl>
2021-05-20 19:23:56 +0200

    inne przykłady

--- 2021-05-20-sprawdzian/przyklad1.py
+++ 2021-05-20-sprawdzian/przyklad1.py
@@ -17,6 +17,7 @@ print("Przykład 1 - jeden wiersz, jedna kolumna:")
 sql = """
     Select count(*)
     From kandydat
+    Where nazwisko Like 'E%'
 """
 print(sql)
 for wiersz in db.execute(sql):
@@ -28,6 +29,7 @@ print("Przykład 2 - jeden wiersz, kilka kolumn:")
 sql = """
     Select min(wiek), max(wiek)
     From kandydat
+    Where imie = 'ALFRED'
 """
 print(sql)
 for wiersz in db.execute(sql):
@@ -43,6 +45,7 @@ sql = """
         wiek > 40
         And wiek < 45
         And imie in ('BORYS', 'ANGELIKA')
+    Order By nazwisko
 """
 print(sql)
 for wiersz in db.execute(sql):
