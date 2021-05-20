a3ffc308688dc3a5839504e92cb0ddc18233e082
Mateusz Adamowski <mateusz@adamowski.pl>
2021-05-20 19:20:30 +0200

    poprawka

--- 2021-05-20-sprawdzian/przyklad1.py
+++ 2021-05-20-sprawdzian/przyklad1.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
 
 import sqlite3
+import urllib.request
+import os
 
+URL = "https://github.com/mateusza2/2020-nlo-inf/raw/master/2021-05-20-sprawdzian/kandydaci.db"
 BAZA = "kandydaci.db"
+
+if not os.path.exists(BAZA):
+    print(f"Pobieram {URL}...")
+    urllib.request.urlretrieve(URL, BAZA)
 
 db = sqlite3.connect(BAZA)
 
-##########################################################
-
 print("Przykład 1 - jeden wiersz, jedna kolumna:")
-
-sql1 = """
+sql = """
     Select count(*)
     From kandydat
 """
-
-for wiersz in db.execute(sql1):
+print(sql)
+for wiersz in db.execute(sql):
     (n,) = wiersz
     print(f"Liczba kandydatów: {n}")
-
-##########################################################
+print("----------");
 
 print("Przykład 2 - jeden wiersz, kilka kolumn:")
-
-sql2 = """
+sql = """
     Select min(wiek), max(wiek)
     From kandydat
 """
-
-for wiersz in db.execute(sql2):
+print(sql)
+for wiersz in db.execute(sql):
     wiek_min, wiek_max = wiersz
     print(f"Przedział wieku: {wiek_min}-{wiek_max}")
-
-##########################################################
+print("----------");
 
 print("Przykład 3 - wiele wierszy, wiele kolumn:")
-
-sql3 = """
+sql = """
     Select imie, nazwisko, rok_ur
     From kandydat
     Where
@@ -44,7 +44,9 @@ sql3 = """
         And wiek < 45
         And imie in ('BORYS', 'ANGELIKA')
 """
-
-for wiersz in db.execute(sql3):
+print(sql)
+for wiersz in db.execute(sql):
     imie, nazwisko, rok = wiersz
     print(f"* {nazwisko} {imie} ({rok})")
+print(f"----------");
+
