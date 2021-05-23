216ff774e5402e00c390534dcf5b1a4609c78978
Mateusz Adamowski <mateusz@adamowski.pl>
2021-05-23 23:47:59 +0200

    3b

---
+++ 2021-05-21-sprawdzian-praktyczny/zadanie3b.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# Autor: Mateusz Adamowski
+# Email: mateusz.adamowski@nlo.edu.pl
+
+import sqlite3
+
+BAZA = "kandydaci.db"
+
+db = sqlite3.connect(BAZA)
+
+print("Ile lat ma w tym roku jedyny kandydat o imieniu 'JOE'?")
+sql = """
+    Select 2021 - rok_ur
+    From kandydat
+    Where imie Like 'JOE'
+"""
+print(sql)
+for wiersz in db.execute(sql):
+    (imie,) = wiersz
+    print(f"{imie}")
